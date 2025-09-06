6c047169427e57935f9ef57a112e92491a7cb298
Matthew Broking <Brokingm@gmail.com>
2025-09-06 16:25:45 -0700

    Added Jump feature - Lecture 3.3
    Added Jump to Character.cpp

--- Source/ActionRoguelike/Private/SCharacter.cpp
+++ Source/ActionRoguelike/Private/SCharacter.cpp
@@ -48,6 +48,7 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 
 	PlayerInputComponent->BindAction("PrimaryAttack", IE_Pressed, this, &ASCharacter::PrimaryAttack);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ASCharacter::Jump);
 }
 
 void ASCharacter::MoveForward(float Value)
